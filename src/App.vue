b566528317855370c22007265e247a22de001ccc
mohamedmehdigara <garamehdi57@yahoo.com>
2023-08-08 09:24:37 +0100

    fixed App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,44 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from "vue";
+
+/* Array to contain our job data */
+const jobs = reactive([]);
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <!-- container for the entire page -->
+  <div class="container">
+    <!-- a div to layout all the jobs in our database -->
+    <div class="jobs">
+      <h1>JOBS</h1>
+      <div class="job" v-for="job in jobs" :key="job.jobUrl">
+        <div class="header">
+          <h2>{{ job.title }}</h2>
+          <span class="material-icons"> delete </span>
+        </div>
+        <h4>{{ job.jobUrl }}</h4>
+        <p>{{ job.desc }}</p>
+        <h3>{{ job.location }}</h3>
+        <h3>{{ job.contact }}</h3>
+        <h3>{{ job.expires }}</h3>
+        <span>posted on {{ job.createdOn}}</span>
+      </div>
+    </div>
+    <!-- a div to layout our form sidebar -->
+    <div class="side">
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+/* These two classes are to layout the title and the delete button on a job */
+.header {
+  display: flex;
+  justify-content: space-between;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.header span {
+  padding: 15px;
+  font-size: 2em;
+  cursor: pointer;
 }
 </style>
